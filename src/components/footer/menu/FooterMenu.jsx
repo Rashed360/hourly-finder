e055d6df444ebeb855ff61a28fe2b9c0a4976378
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-02-02 00:44:08 +0600

    Create Footer Menu List Array, then apply map function on FooterMenu Component

---
+++ src/components/footer/menu/FooterMenu.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const FooterMenu = ({menu}) => {
+    return (
+        <div className='col-lg-3'>
+            <div className='footer-menu-wrapper'>
+                <div className='section-title'>
+                    <h3>{menu.title}</h3>
+                </div>
+                <div className='footer-menu'>
+                    <nav>
+                        <ul>
+                            {
+                                menu.lists.map(list => <li>
+                                    <Link to={list.link}>{list.name}</Link>
+                                </li>)
+                            }
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FooterMenu;
