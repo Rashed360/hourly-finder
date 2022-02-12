36b06666988ae42006bcd22a05ab21c7faabdca4
Rashed Ahmed <rashed.ahmed@email.com>
2022-02-12 22:09:20 +0600

    auto-complete feature added

--- src/components/home/heroSection/searchBar/SearchBar.js
+++ src/components/home/heroSection/searchBar/SearchBar.js
@@ -1,20 +1,74 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import SearchBarTag from './SearchBarTag'
 import { FaMapMarkerAlt } from 'react-icons/fa'
 
 const SearchBar = () => {
+	const [districts, setDistricts] = useState([])
+	const [suggestions, setSuggestions] = useState([])
+	const [showSuggestion, setShowSuggestion] = useState(false)
+	const [text, setText] = useState('')
+
+	useEffect(() => {
+		const loadDistricts = async () => {
+			const response = await axios.get(
+				'https://bdapis.herokuapp.com/api/v1.1/districts'
+			)
+			setDistricts(response.data.data)
+		}
+		loadDistricts()
+	}, [])
+
+	const onChangeHandler = text => {
+		let matches = []
+		if (text.length > 0) {
+			matches = districts.filter(dst => {
+				const regex = new RegExp(`${text}`, 'gi')
+				return dst.district.match(regex)
+			})
+		}
+		if (matches.length > 0) {
+			setSuggestions(matches)
+			setText(text)
+			setShowSuggestion(true)
+		} else {
+			setSuggestions([])
+			setText(text)
+			setShowSuggestion(false)
+		}
+	}
+
+	const onSuggestHandler = text => {
+		setText(text)
+		setSuggestions([])
+	}
+
 	return (
-		<div className='hero-search-bar active'>
+		<div className={showSuggestion ? 'hero-search-bar active' : 'hero-search-bar'}>
 			<div className='search-bar'>
-                <FaMapMarkerAlt className='icon' />
-                <input type='text' placeholder='Where do you live ?' />
-                <button className='btn'>Get Started</button>
+				<FaMapMarkerAlt className='icon' />
+				<input
+					type='text'
+					onChange={e => onChangeHandler(e.target.value)}
+					onBlur={() => {
+						setTimeout(() => {
+							setShowSuggestion(false)
+						}, 100)
+					}}
+					value={text}
+					placeholder='Where do you live ?'
+				/>
+				<button className='btn'>Get Started</button>
 			</div>
 
-            <div className="auto-complete">
-                    <li>Hello</li>
-                    <li>Hello</li>
-                    <li>Hello</li>
-            </div>
+			<div className='auto-complete'>
+				{suggestions &&
+					suggestions.map((sug, i) => (
+						<li key={i} onClick={() => onSuggestHandler(sug.district)}>
+							{sug.district}
+						</li>
+					))}
+			</div>
 
 			<SearchBarTag />
 		</div>
