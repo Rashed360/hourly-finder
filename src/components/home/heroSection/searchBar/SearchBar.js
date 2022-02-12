f539d6914037a791249381e12fec55d327357c91
Rashed Ahmed <rashed.ahmed@email.com>
2022-02-12 19:59:19 +0600

    button auto-complete fix

--- src/components/home/heroSection/searchBar/SearchBar.js
+++ src/components/home/heroSection/searchBar/SearchBar.js
@@ -3,9 +3,8 @@ import { FaMapMarkerAlt } from 'react-icons/fa'
 
 const SearchBar = () => {
 	return (
-		<div className='hero-search-bar'>
-
-			<div className='search-bar active'>
+		<div className='hero-search-bar active'>
+			<div className='search-bar'>
                 <FaMapMarkerAlt className='icon' />
                 <input type='text' placeholder='Where do you live ?' />
                 <button className='btn'>Get Started</button>
