650ed7191f8776a3d19cde870337d5f6d2e120db
Rashed Ahmed <rashed.ahmed@email.com>
2022-03-14 22:21:30 +0600

    tweaks on job block to fit transparent pngs

--- src/components/commonComponents/tagElement/Tags.js
+++ src/components/commonComponents/tagElement/Tags.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Tags = ({ tags }) => {
-  return (
-    <div className="blog-keyword">
-      {tags.map((tag,i) => {
-        return (
-          <Link to="" className="blog-tag" key={i}>
-            <div className={tag.color}></div> {tag.name}
-          </Link>
-        )
-      })}
-    </div>
-  )
+	return (
+		<div className='blog-keyword'>
+			{tags.map((tag, i) => {
+				return (
+					<Link to='' className='blog-tag' key={i}>
+						<div style={{ backgroundColor: tag.color }}></div> {tag.name}
+					</Link>
+				)
+			})}
+		</div>
+	)
 }
 
 export default Tags
--- src/components/singleJob/jobSideBar/JobSideBar.js
+++ src/components/singleJob/jobSideBar/JobSideBar.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaEnvelope, FaMapMarkerAlt, FaBriefcase } from 'react-icons/fa'
 
