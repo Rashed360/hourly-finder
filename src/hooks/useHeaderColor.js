4522dea1b4a8ed21d82bcf48754967a0165e42c1
Rashed Ahmed <rashed.ahmed@email.com>
2022-03-19 14:24:01 +0600

    useHeaderColor hook

--- src/hooks/useHeaderColor.js
+++ src/hooks/useHeaderColor.js
@@ -4,9 +4,14 @@ const useHeaderColor = () => {
 	const { pathname } = useLocation()
 	let headerColor = 'transparent'
 
-    if (pathname.includes('/dashboard')) {
-        headerColor = '#d1e4e9'
-    }
+	if (pathname.includes('/dashboard')) {
+		headerColor = 'linear-gradient(to right, #fafafa, #d1e4e9)'
+		// headerColor = '#d1e4e9'
+	} else if (pathname.includes('/contact')) {
+		headerColor = '#fafafa'
+	} else if (pathname.includes('/about')) {
+		headerColor = 'linear-gradient(to bottom, lightgrey, #fafafa)'
+	}
 
 	return headerColor
 }
