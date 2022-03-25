2733b59f280af1e2c77e27e4c256bcb09865f2af
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-03-25 23:08:37 +0600

    All job page connect into redux and fetch all job

--- src/components/allJobs/AllJobs.js
+++ src/components/allJobs/AllJobs.js
@@ -1,14 +1,41 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import JobBlock from "../commonComponents/commonBlock/jobBlock/JobBlock"
+import JobBlockSkeleton from "../commonComponents/skeletons/JobBlockSkeleton"
+import { jobAllFetch } from "./../../redux/actionCreators/jobActionCreators"
 import JobFilterArea from "./filterArea/JobFilterArea"
 import AllJobContent from "./jobContent/AllJobContent"
 import JobSearchBar from "./searchBar/JobSearchBar"
 
 const AllJobs = () => {
+  const dispatch = useDispatch()
+  const allJobs = useSelector((state) => state.job.allJobs)
+  useEffect(() => {
+    dispatch(jobAllFetch())
+  }, [])
+
+  const jobs = allJobs?.map((job, index) => {
+    return (
+      <div className='col-lg-4' key={index}>
+        <JobBlock job={job} />
+      </div>
+    )
+  })
+
+  const jobsSkeleton = [1, 2, 3, 4, 5, 6, 7, 8].map((index) => {
+    return (
+      <div className='col-lg-4' key={index}>
+        <JobBlockSkeleton />
+      </div>
+    )
+  })
+
   return (
     <div>
       <JobSearchBar />
       <div className='filter-header-area'>
         <JobFilterArea />
-        <AllJobContent />
+        <AllJobContent allJobs={allJobs} jobs={jobs} jobsSkeleton={jobsSkeleton} />
       </div>
     </div>
   )
--- src/components/allJobs/jobContent/AllJobContent.js
+++ src/components/allJobs/jobContent/AllJobContent.js
@@ -1,9 +1,8 @@
 import React from "react"
 import { FaServer, FaSortAmountDown } from "react-icons/fa"
-import { Link } from "react-router-dom"
 import Pagination from "../pagination/Pagination"
 
-const AllJobContent = () => {
+const AllJobContent = ({ jobs, allJobs, jobsSkeleton }) => {
   return (
     <div className='content-area'>
       <div className='grid-header d-flex justify-content-between'>
@@ -11,7 +10,7 @@ const AllJobContent = () => {
           <span>
             <FaServer />
           </span>
-          <span className='filter-header-text'>Showing 176 Total Jobs Nearby</span>
+          <span className='filter-header-text'>Showing {allJobs.length} Total Jobs Nearby</span>
         </div>
         <div className='filter-header right'>
           <span className='filter-header-text'>Sort : Newest first</span>
@@ -22,35 +21,8 @@ const AllJobContent = () => {
       </div>
       <div className='all-filtered-job'>
         <div className='row'>
-          <div className='col-lg-4'>
-            <div className='job-card'>
-              <div className='about-recruiter d-flex align-items-center'>
-                <Link to=''>
-                  <img src='images/homepage/recruiter-logo.png' alt='logo' className='recruiter-logo' />
-                </Link>
-                <h5 className='recruiter-title'>
-                  <Link to=''>Founders,</Link>
-                  <span className='post_time'>20min ago</span>
-                </h5>
-              </div>
-              <div className='job-content'>
-                <h3 className='job-title'>
-                  <Link to=''>Complete House Paintings Services</Link>
-                </h3>
-                <div className='job-status'>
-                  <span className='job-type'>Hourly</span> in
-                  <span className='job-location'>
-                    <Link to=''> Banani</Link>
-                  </span>
-                </div>
-                <div className='job-keyword'>
-                  <Link to='' className='job-tag'>
-                    <div className='creative'></div> Creative
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+          {allJobs === null && jobsSkeleton}
+          {allJobs !== null && jobs}
         </div>
       </div>
 
