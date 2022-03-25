f2aefb625390fac5d9859701adc74a1dca2fd82e
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-03-26 03:18:02 +0600

    conditional render in Edit Profile of Dashboard

--- src/components/home/newsletterSection/NewsLetterSection.js
+++ src/components/home/newsletterSection/NewsLetterSection.js
@@ -1,84 +1,84 @@
-import { Formik, Form, Field } from 'formik'
-import { FaEnvelope, FaRegLaugh,FaRegTired,FaRegKissWinkHeart  } from 'react-icons/fa'
+import { Field, Form, Formik } from "formik"
+import { FaEnvelope, FaRegKissWinkHeart, FaRegLaugh, FaRegTired } from "react-icons/fa"
 
 const NewsLetterSection = () => {
-	const newletterContent = {
-		textContent: {
-			title: 'Subscribe to receive our weekly blog',
-		},
-	}
+  const newletterContent = {
+    textContent: {
+      title: "Subscribe to receive our weekly blog",
+    },
+  }
 
-	const initialValues = {
-		newsletterEmail: '',
-	}
+  const initialValues = {
+    newsletterEmail: "",
+  }
 
-	const onSubmitHandle = (values,{resetForm}) => {
-		console.log('Subscribed:', values.newsletterEmail)
-		resetForm(initialValues)
-	}
+  const onSubmitHandle = (values, { resetForm }) => {
+    console.log("Subscribed:", values.newsletterEmail)
+    resetForm(initialValues)
+  }
 
-	const validateHandle = values => {
-		const errors = {}
+  const validateHandle = (values) => {
+    const errors = {}
 
-		if (!values.newsletterEmail) {
-			errors.newsletterEmail = 'Field is Required'
-		} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.newsletterEmail)) {
-			errors.newsletterEmail = 'Invalid Email Address'
-		}
-		return errors
-	}
+    if (!values.newsletterEmail) {
+      errors.newsletterEmail = "Field is Required"
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.newsletterEmail)) {
+      errors.newsletterEmail = "Invalid Email Address"
+    }
+    return errors
+  }
 
-	return (
-		<div className='newsletter-area section-padding'>
-			<div className='container'>
-				<div className='row'>
-					<div className='col-lg-6 offset-lg-3'>
-						<div className='newsletter-content'>
-							<h4>{newletterContent.textContent.title}</h4>
-							<Formik initialValues={initialValues} onSubmit={onSubmitHandle} validate={validateHandle}>
-								{({ values, errors, touched, handleChange, handleSubmit, handleReset }) => (
-									<Form onSubmit={handleSubmit} onReset={handleReset} className='newsletter-form'>
-										<Field
-											name='newsletterEmail'
-											type='text'
-											className={
-												touched.newsletterEmail
-													? errors.newsletterEmail
-														? 'form-control newsletter-email is-invalid'
-														: 'form-control newsletter-email is-valid'
-													: 'form-control newsletter-email'
-											}
-											value={values.newsletterEmail}
-											onChange={handleChange}
-											placeholder='Your Email Address'
-										/>
-										<FaEnvelope className='svg' />
-										<input type='submit' value='Subscribe' className='btn newsletter' />
+  return (
+    <div className='newsletter-area section-padding'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-6 offset-lg-3'>
+            <div className='newsletter-content'>
+              <h4>{newletterContent.textContent.title}</h4>
+              <Formik initialValues={initialValues} onSubmit={onSubmitHandle} validate={validateHandle}>
+                {({ values, errors, touched, handleChange, handleSubmit, handleReset }) => (
+                  <Form onSubmit={handleSubmit} onReset={handleReset} className='newsletter-form'>
+                    <Field
+                      name='newsletterEmail'
+                      type='text'
+                      className={
+                        touched.newsletterEmail
+                          ? errors.newsletterEmail
+                            ? "form-control newsletter-email is-invalid"
+                            : "form-control newsletter-email is-valid"
+                          : "form-control newsletter-email"
+                      }
+                      value={values.newsletterEmail}
+                      onChange={handleChange}
+                      placeholder='Your Email Address'
+                    />
+                    <FaEnvelope className='svg' />
+                    <input type='submit' value='Subscribe' className='btn btn-main newsletter' />
 
-										{touched.newsletterEmail ? (
-											errors.newsletterEmail ? (
-												<p className='err'>
-													{errors.newsletterEmail} <FaRegTired />
-												</p>
-											) : (
-												<p className='okk'>
-													Looks Good! <FaRegKissWinkHeart />
-												</p>
-											)
-										) : (
-											<p>
-												Dont worry we dont spam! <FaRegLaugh />
-											</p>
-										)}
-									</Form>
-								)}
-							</Formik>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+                    {touched.newsletterEmail ? (
+                      errors.newsletterEmail ? (
+                        <p className='err'>
+                          {errors.newsletterEmail} <FaRegTired />
+                        </p>
+                      ) : (
+                        <p className='okk'>
+                          Looks Good! <FaRegKissWinkHeart />
+                        </p>
+                      )
+                    ) : (
+                      <p>
+                        Dont worry we dont spam! <FaRegLaugh />
+                      </p>
+                    )}
+                  </Form>
+                )}
+              </Formik>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NewsLetterSection
