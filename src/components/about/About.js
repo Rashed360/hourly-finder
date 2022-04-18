68001323dd7ad2e1f0582de8373407518b4a7cf3
Rashed Ahmed <rashed.ahmed@email.com>
2022-04-19 00:38:22 +0600

    update aboutUs with information

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -10,26 +10,33 @@ const About = () => {
 	const teamMembersInfo = [
 		{
 			id: 1,
-			name: 'Sajeeb Debnath',
-			designation: 'Founder',
-			image: '/images/aboutPage/team-1.jpg'
+			name: 'Narayan Ranjan Chakraborty',
+			designation: 'Supervisor',
+			image: '/images/aboutPage/team-3.jpg',
 		},
 		{
 			id: 2,
+			name: 'Ms. Subhenur Latif',
+			designation: 'Co-Supervisor',
+			image: '/images/aboutPage/team-4.jpg',
+		},
+		{
+			id: 3,
+			name: 'Sajeeb Debnath',
+			designation: 'Team Member',
+			image: '/images/aboutPage/team-1.jpg',
+		},
+		{
+			id: 4,
 			name: 'Rashed Ahmed',
-			designation: 'Co-Founder',
-			image: '/images/aboutPage/team-2.jpg'
+			designation: 'Team Member',
+			image: '/images/aboutPage/team-2.jpg',
 		},
 	]
 
 	const teamMembers = teamMembersInfo.map(member => {
 		return (
-			<MemberCard 
-				key={member.id}
-				name={member.name}
-				designation={member.designation}
-				image={member.image}
-			/>
+			<MemberCard key={member.id} name={member.name} designation={member.designation} image={member.image} />
 		)
 	})
 
@@ -38,100 +45,80 @@ const About = () => {
 			id: 1,
 			month: 'Apr',
 			day: '20',
-			title: 'Initial Plannings'
+			title: 'Initial Plans',
 		},
 		{
 			id: 2,
 			month: 'Jun',
 			day: '12',
-			title: 'UI/UX &amp; Brainstorming'
+			title: 'UI/UX Design',
 		},
 		{
 			id: 3,
 			month: 'Aug',
 			day: '23',
-			title: 'Front End'
+			title: 'Front End',
 		},
 		{
 			id: 4,
 			month: 'Oct',
 			day: '04',
-			title: 'Back End'
-		}
+			title: 'Back End',
+		},
 	]
 
 	const roadmap = roadmapInfo.map(info => {
-		return (
-			<RoadmapCard
-				key={info.id}
-				month={info.month}
-				day={info.day}
-				title={info.title}
-			/>
-		)
+		return <RoadmapCard key={info.id} month={info.month} day={info.day} title={info.title} />
 	})
 
-
 	const stories_plan = {
-		our_story : {
-			title : "Our Story",
-			description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit vel, urna imperdiet aliquet platea sodales. Faucibus congue vulputate nunc, facilisi diam volutpat augue. Luctus dui a mauris, diam eget nisl, vel vel. Etiam in ullamcorper neque aliquet malesuada pharetra est neque.",
-			img : StoryBg
+		our_story: {
+			title: 'Our Story',
+			description:
+				'The story behind making our platform is to help students find hourly-based jobs nearby and help recruiters to find employees at ease. Thus, students can utilize their free time and bear their own expenses, and employers can save their valuable time. Our goal is to create an attitude among people that "Small or Large, No Job Is Negligible."',
+			img: StoryBg,
 		},
-		future_plan : {
-			title : "Future Plans",
-			description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit vel, urna imperdiet aliquet platea sodales. Faucibus congue vulputate nunc, facilisi diam volutpat augue. Luctus dui a mauris, diam eget nisl, vel vel. Etiam in ullamcorper neque aliquet malesuada pharetra est neque.",
-			img : FutureBg
-		}
+		future_plan: {
+			title: 'Future Plans',
+			description:
+				"Hourly-Finder platform started as a project for our university's final year defense, but we had more effective plans for the project from the beginning. We will work hard in the future to deliver the platform to each and every person in our country so that they can get the most result from it.",
+			img: FutureBg,
+		},
 	}
 
 	return (
 		<div>
-			<div
-				className='intro-area center-flex mt-70'>
+			<div className='intro-area center-flex mt-70'>
 				<div className='intro-text'>
 					<h1 className='intro-heading'>Hourly Finder</h1>
 					<p>
-						A project by team CodeDamn and <br/>a platform to find hourly-based jobs
-						nearby via live location.
+						A project by team CodeDamn and <br />a platform to find hourly-based jobs nearby via live
+						location.
 					</p>
 				</div>
 			</div>
-			{/* <!-- ==================== Intro Area End ==================== --> */}
 
-			{/* <!-- ==================== Story Area Start ==================== --> */}
-			<StoryPlan story={stories_plan.our_story} order="" />
-			{/* <!-- ==================== Story Area End ==================== --> */}
+			<StoryPlan story={stories_plan.our_story} order='' />
 
-			{/* <!-- ==================== Team Area Start ==================== --> */}
 			<div className='team-area center-flex'>
 				<div className='team-content'>
 					<h3 className='team-heading'>Our Team</h3>
-					<div className='team-cards'>
-						{teamMembers}
-					</div>
+					<div className='team-cards'>{teamMembers}</div>
 				</div>
 			</div>
-			{/* <!-- ==================== Team Area End ==================== --> */}
 
-			{/* <!-- ==================== Future Area Start ==================== --> */}
-			<StoryPlan story={stories_plan.future_plan} order="order-first" />
-			{/* <!-- ==================== Future Area End ==================== --> */}
+			<StoryPlan story={stories_plan.future_plan} order='order-first' />
 
-			{/* <!-- ==================== Roadmap Area Start ==================== --> */}
 			<div className='roadmap-area center-flex'>
 				<div className='roadmap-content'>
 					<h3 className='roadmap-heading'>Road Map</h3>
 
-					<div className='roadmap-cards'>
-					{roadmap}
-					</div>
+					<div className='roadmap-cards'>{roadmap}</div>
 					<div className='roadmap-road'>
 						<div className='roadmap-progress'></div>
 					</div>
 				</div>
 			</div>
-			{/* <!-- ==================== Roadmap Area End ==================== --> */}
 		</div>
 	)
 }
