5c6f29c6ab587f8685c2371e4326230b3e511fb6
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-04-22 04:29:48 +0600

    few change in blog Reducer

--- src/components/allJobs/pagination/Pagination.js
+++ src/components/allJobs/pagination/Pagination.js
@@ -1,54 +1,55 @@
-import { Link } from 'react-router-dom'
-import { useSearchParams } from 'react-router-dom'
-import { jobAllPaginationNavigate } from '../../../redux/actionCreators/jobActionCreators'
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
-import { useSelector, useDispatch } from 'react-redux'
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useSearchParams } from "react-router-dom"
+import { jobAllPaginationNavigate } from "../../../redux/actionCreators/jobActionCreators"
 
 const Pagination = () => {
-	const dispatch = useDispatch()
-	const [searchParams, setSearchParams] = useSearchParams()
-	const allJobsPagination = useSelector(state => state.job.allJobsPagination)
-	const previous = allJobsPagination?.previous
-	const next = allJobsPagination?.next
-	const page = searchParams.get('page')
+  const dispatch = useDispatch()
+  const [searchParams, setSearchParams] = useSearchParams()
 
-	const paginate = curPage => {
-		if (curPage === 1) {
-			dispatch(jobAllPaginationNavigate(next))
-			setSearchParams({ page: '' })
-			console.log('->', page)
-		} else {
-			dispatch(jobAllPaginationNavigate(previous))
-			setSearchParams({ page: '' })
-			console.log('<-', page)
-		}
-	}
+  const allJobsPagination = useSelector((state) => state.job.allJobsPagination)
+  const previous = allJobsPagination?.previous
+  const next = allJobsPagination?.next
+  const page = searchParams.get("page")
+  console.log(next)
 
-	return (
-		<div className='pagination-wrapper'>
-			<a onClick={() => paginate(0)} className={previous === null ? 'page disabled' : 'page'}>
-				<FaArrowLeft />
-				Prev
-			</a>
+  const paginate = (curPage) => {
+    if (curPage === 1) {
+      dispatch(jobAllPaginationNavigate(next))
+      setSearchParams({ page: "" })
+      console.log("->", page)
+    } else {
+      dispatch(jobAllPaginationNavigate(previous))
+      setSearchParams({ page: "" })
+      console.log("<-", page)
+    }
+  }
 
-			<Link to='' className='link'>
-				1
-			</Link>
+  return (
+    <div className='pagination-wrapper'>
+      <a onClick={() => paginate(0)} className={previous === null ? "page disabled" : "page"}>
+        <FaArrowLeft />
+        Prev
+      </a>
 
-			<Link to='' className='link'>
-				2
-			</Link>
+      <Link to='' className='link'>
+        1
+      </Link>
 
-			<Link to='' className='link'>
-				3
-			</Link>
+      <Link to='' className='link'>
+        2
+      </Link>
 
-			<a onClick={() => paginate(1)} className={next === null ? 'page next disabled' : 'page next'}>
-				Next
-				<FaArrowRight />
-			</a>
-		</div>
-	)
+      <Link to='' className='link'>
+        3
+      </Link>
+
+      <a onClick={() => paginate(1)} className={next === null ? "page next disabled" : "page next"}>
+        Next
+        <FaArrowRight />
+      </a>
+    </div>
+  )
 }
 
 export default Pagination
--- src/redux/reducers/blogReducer.js
+++ src/redux/reducers/blogReducer.js
@@ -1,14 +1,14 @@
 const blogInitialState = {
-    allBlog: null,
-	allBlogPagination: null,
-	allBlogFailed: false,
-	singleBlogLoading: false,
-	singleBlogFailed: false,
-	singleBlog: null,
+  allBlog: null,
+  allBlogPagination: null,
+  allBlogFailed: false,
+  singleBlogLoading: false,
+  singleBlogFailed: false,
+  singleBlog: null,
 }
 
 const blogReducer = (state = blogInitialState, action) => {
-
+  return state
 }
 
-export default blogReducer;
+export default blogReducer
