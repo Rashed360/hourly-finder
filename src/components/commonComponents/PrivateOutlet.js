373e2bcff33c83eaa73128d22271c7e07a39d1f2
Rashed Ahmed <rashed.ahmed@email.com>
2022-04-23 02:15:09 +0600

    move privateOutlet to components

--- src/components/commonComponents/PrivateOutlet.js
+++ src/components/commonComponents/PrivateOutlet.js
@@ -1,5 +1,5 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom'
-import useAuth from '../hooks/useAuth'
+import useAuth from '../../hooks/useAuth'
 
 const PrivateOutlet = () => {
   const location = useLocation()
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,28 +1,23 @@
-import React from "react"
-import ContactFormSection from "./contactFormSection/ContactFormSection"
-import ContactRightSection from "./contactRightbar/ContactRightSection"
-import FaqSection from "./faqSection/FaqSection"
+import ContactFormSection from './contactFormSection/ContactFormSection'
+import ContactRightSection from './contactRightbar/ContactRightSection'
+import FaqSection from './faqSection/FaqSection'
 
 const Contact = () => {
 	document.title = 'HourlyFinder | Contact Us'
 
 	return (
-		<div>
-			{/* <!-- ==================== Contact Area Start ==================== --> */}
-			<div className='contact-area mt-70'>
-				<div className='container'>
-					<div className='row'>
-						<div className='col-lg-8'>
-							<ContactFormSection></ContactFormSection>
-							<FaqSection></FaqSection>
-						</div>
-						<div className='col-lg-4'>
-							<ContactRightSection></ContactRightSection>
-						</div>
+		<div className='contact-area mt-70'>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-lg-8'>
+						<ContactFormSection></ContactFormSection>
+						<FaqSection></FaqSection>
+					</div>
+					<div className='col-lg-4'>
+						<ContactRightSection />
 					</div>
 				</div>
 			</div>
-			{/* <!-- ==================== Contact Area End ==================== --> */}
 		</div>
 	)
 }
