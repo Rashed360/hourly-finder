1a72f320dd57e45050494d9f747fcf7e6f0d470e
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-05-01 02:21:50 +0600

    Responsive Mobile Menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,144 +1,175 @@
-import { useEffect, useState } from 'react'
-import { Dropdown } from 'react-bootstrap'
-import { FaBell } from 'react-icons/fa'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom'
-import Image from '../../assets/images/user.svg'
-import Logo from '../../assets/logos/logo.svg'
-import useHeaderColor from '../../hooks/useHeaderColor'
-import Animate from './animate/Animate'
-import { userFetch } from '../../redux/actionCreators/userActionCreators'
-import ModalYesNo from 'components/commonComponents/modals/ModalYesNo'
+import ModalYesNo from "components/commonComponents/modals/ModalYesNo"
+import { useEffect, useState } from "react"
+import { Dropdown } from "react-bootstrap"
+import { FaBell } from "react-icons/fa"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom"
+import Image from "../../assets/images/user.svg"
+import Logo from "../../assets/logos/logo.svg"
+import useHeaderColor from "../../hooks/useHeaderColor"
+import { userFetch } from "../../redux/actionCreators/userActionCreators"
+import Animate from "./animate/Animate"
 
 const Header = () => {
-	const [logoutModal, setLogoutModal] = useState(false)
-	const dispatch = useDispatch()
-	const navigate = useNavigate()
-	const token = useSelector(state => state.auth.token)
-	const user = useSelector(state => state.user.user)
-	const profile = useSelector(state => state.user.profile)
-	const picture = profile?.picture
-	const headerColor = useHeaderColor()
-	const [navbar, setNavbar] = useState()
-	const { pathname } = useLocation()
+  const [logoutModal, setLogoutModal] = useState(false)
+  const [isNavExpan, setIsNavExpan] = useState(false)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const token = useSelector((state) => state.auth.token)
+  const user = useSelector((state) => state.user.user)
+  const profile = useSelector((state) => state.user.profile)
+  const picture = profile?.picture
+  const headerColor = useHeaderColor()
+  const [navbar, setNavbar] = useState()
+  const { pathname } = useLocation()
 
-	const STYLES = {
-		normal: {
-			background: headerColor,
-			transition: 'background 500ms',
-		},
-		active: {
-			background: 'white',
-		},
-	}
+  const mainMenu = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Jobs",
+      link: "/jobs",
+    },
+    {
+      name: "Hire",
+      link: "/jobseeker",
+    },
+    {
+      name: "Blog",
+      link: "/blogs",
+    },
+    {
+      name: "About",
+      link: "/about",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ]
 
-	useEffect(() => {
-		if (token) {
-			dispatch(userFetch())
-		}
-		scrollToZero()
-	}, [pathname, token, dispatch])
+  const STYLES = {
+    normal: {
+      background: headerColor,
+      transition: "background 500ms",
+    },
+    active: {
+      background: "white",
+    },
+  }
 
-	const changeNavbar = () => {
-		if (window.scrollY >= 1) {
-			setNavbar(true)
-		} else {
-			setNavbar(false)
-		}
-	}
-	const scrollToZero = () => {
-		window.scrollTo(0, 0)
-	}
-	const logoutAction = () => {
-		navigate('/logout')
-	}
-	window.addEventListener('scroll', changeNavbar)
+  useEffect(() => {
+    if (token) {
+      dispatch(userFetch())
+    }
+    scrollToZero()
+  }, [pathname, token, dispatch])
 
-	return (
-		<div
-			className={navbar ? 'header-top-area active' : 'header-top-area'}
-			style={navbar ? STYLES.active : STYLES.normal}
-		>
-			<div className='container-fluid'>
-				<div className='row d-flex justify-content-center'>
-					<div className='col-lg-3'>
-						<div className='logo'>
-							<Animate />
-							<Link to='/' onClick={scrollToZero}>
-								<img src={Logo} alt='Logo' />
-							</Link>
-						</div>
-					</div>
-					<div className='d-flex justify-content-end col-lg-9'>
-						<div className='menu-area'>
-							<nav>
-								<ul>
-									<li>
-										<NavLink to='/'>Home</NavLink>
-									</li>
-									<li>
-										<NavLink to='/jobs'>Jobs</NavLink>
-									</li>
-									<li>
-										<NavLink to='/jobseeker'>Hire</NavLink>
-									</li>
-									<li>
-										<NavLink to='/blogs'>Blog</NavLink>
-									</li>
-									<li>
-										<NavLink to='/about'>About</NavLink>
-									</li>
-									<li>
-										<NavLink to='/contact'>Contact</NavLink>
-									</li>
-								</ul>
-							</nav>
-							{token === null ? (
-								<Link to='/login' className='btn btn-main join-us'>
-									Join Us
-								</Link>
-							) : (
-								<div className='header-icon'>
-									<Link to='/dashboard'>
-										<span>
-											<FaBell />
-										</span>
-									</Link>
+  const changeNavbar = () => {
+    if (window.scrollY >= 1) {
+      setNavbar(true)
+    } else {
+      setNavbar(false)
+    }
+  }
+  const scrollToZero = () => {
+    window.scrollTo(0, 0)
+  }
+  const logoutAction = () => {
+    navigate("/logout")
+  }
+  window.addEventListener("scroll", changeNavbar)
 
-									<Dropdown>
-										<Dropdown.Toggle as='a'>
-											<img src={picture ? picture : Image} alt='' />
-										</Dropdown.Toggle>
+  return (
+    <div
+      className={navbar ? "header-top-area active" : "header-top-area"}
+      style={navbar ? STYLES.active : STYLES.normal}
+    >
+      <div className='navigation'>
+        <div className='logo'>
+          <Animate />
+          <Link to='/' onClick={scrollToZero}>
+            <img src={Logo} alt='Logo' />
+          </Link>
+        </div>
+        <button className='menu-trigger' onClick={() => setIsNavExpan(!isNavExpan)}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            aria-hidden='true'
+            role='img'
+            width='1em'
+            height='1em'
+            preserveAspectRatio='xMidYMid meet'
+            viewBox='0 0 24 24'
+          >
+            <path
+              fill='none'
+              stroke='currentColor'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+              d='M4 6h16M4 12h16m-7 6h7'
+            />
+          </svg>
+        </button>
+        <div className={isNavExpan ? "menu-area expand" : "menu-area"}>
+          <nav>
+            <ul>
+              {mainMenu.map((menu, index) => {
+                return (
+                  <li key={index} onClick={() => setIsNavExpan(!isNavExpan)}>
+                    <NavLink to={menu.link}>{menu.name}</NavLink>
+                  </li>
+                )
+              })}
+            </ul>
+          </nav>
+          {token === null ? (
+            <Link to='/login' className='btn btn-main join-us' onClick={() => setIsNavExpan(!isNavExpan)}>
+              Join Us
+            </Link>
+          ) : (
+            <div className='header-icon'>
+              <Link to='/dashboard'>
+                <span>
+                  <FaBell />
+                </span>
+              </Link>
 
-										<Dropdown.Menu>
-											<Link to='/dashboard'>Dashboard</Link>
-											{user?.user_type === 2 && <Link to='/jobseeker'>Jobseeker</Link>}
-											<Link to='/dashboard/message'>Message</Link>
-											<Link to='#' onClick={() => setLogoutModal(true)}>
-												Logout
-											</Link>
-										</Dropdown.Menu>
-									</Dropdown>
-								</div>
-							)}
-						</div>
-					</div>
-				</div>
-			</div>
-			<ModalYesNo
-				open={logoutModal}
-				onClose={() => setLogoutModal(false)}
-				onAccept={logoutAction}
-				data={{
-					title: 'Are you sure to Logout?',
-					subTitle: 'Private data will be unaccessible.',
-					body: 'Logout from authentication session',
-					footer: '* You have to login again to access private data!',
-					yesText: 'Logout',
-				}}
-			/>
-		</div>
-	)
+              <Dropdown>
+                <Dropdown.Toggle as='a'>
+                  <img src={picture ? picture : Image} alt='' />
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Link to='/dashboard'>Dashboard</Link>
+                  {user?.user_type === 2 && <Link to='/jobseeker'>Jobseeker</Link>}
+                  <Link to='/dashboard/message'>Message</Link>
+                  <Link to='#' onClick={() => setLogoutModal(true)}>
+                    Logout
+                  </Link>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          )}
+        </div>
+      </div>
+      <ModalYesNo
+        open={logoutModal}
+        onClose={() => setLogoutModal(false)}
+        onAccept={logoutAction}
+        data={{
+          title: "Are you sure to Logout?",
+          subTitle: "Private data will be unaccessible.",
+          body: "Logout from authentication session",
+          footer: "* You have to login again to access private data!",
+          yesText: "Logout",
+        }}
+      />
+    </div>
+  )
 }
 
 export default Header
