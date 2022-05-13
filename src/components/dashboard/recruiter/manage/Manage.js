6b75503b044cc521f682c5fb844a2f8c25795551
Rashed Ahmed <rashed.ahmed@email.com>
2022-05-13 20:55:28 +0600

    added offer form to hire

--- src/components/dashboard/recruiter/manage/Manage.js
+++ src/components/dashboard/recruiter/manage/Manage.js
@@ -5,8 +5,8 @@ import SeekerApplication from '../../seeker/application/SeekerApplication'
 const Manage = () => {
 	const user = useSelector(state => state.user.user)
 
-	if (user.user_type === 2) return <RecruiterPostedJob />
-	else if (user.user_type === 1) return <SeekerApplication />
+	if (user?.user_type === 2) return <RecruiterPostedJob />
+	else if (user?.user_type === 1) return <SeekerApplication />
 	else return <p>Invalid User</p>
 }
 
