e3d0d3cade8ef36d2d76da438d80241f2eb78d4c
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-05-13 21:14:07 +0600

    update design for ongoing job card

--- src/components/dashboard/recruiter/ongoing/recruiter/RecruiterOngoingJob.js
+++ src/components/dashboard/recruiter/ongoing/recruiter/RecruiterOngoingJob.js
@@ -1,56 +1,56 @@
-import axios from 'axios'
-import { useSelector, useDispatch } from 'react-redux'
-import { useState, useEffect } from 'react'
-import RecruiterOngoingJobBlock from './RecruiterOngoingJobBlock'
-import { Spinner } from 'react-bootstrap'
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Spinner } from "react-bootstrap"
+import { useDispatch, useSelector } from "react-redux"
+import RecruiterOngoingJobBlock from "./RecruiterOngoingJobBlock"
 const url = process.env.REACT_APP_BACKEND_SERVER
 
 const RecruiterOngoingJob = () => {
-	document.title = 'HourlyFinder | Ongoing Jobs'
-	const [ongoingJobs, setOngoingJobs] = useState(null)
-	const dispatch = useDispatch()
-	const profile = useSelector(state => state.user.profile)
-	const { id } = profile || { id: null }
+  document.title = "HourlyFinder | Ongoing Jobs"
+  const [ongoingJobs, setOngoingJobs] = useState(null)
+  const dispatch = useDispatch()
+  const profile = useSelector((state) => state.user.profile)
+  const { id } = profile || { id: null }
 
-	const fetchOngoingJobs = async () => {
-		const token = localStorage.getItem('token')
-		if (token) {
-			const config = {
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `JWT ${token}`,
-					Accept: 'application/json',
-				},
-			}
-			await axios
-				.get(`${url}/jobs/work/?id=${id}`, config)
-				.then(response => setOngoingJobs(response.data))
-				.catch(error => console.log(error.response))
-		}
-	}
+  const fetchOngoingJobs = async () => {
+    const token = localStorage.getItem("token")
+    if (token) {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${token}`,
+          Accept: "application/json",
+        },
+      }
+      await axios
+        .get(`${url}/jobs/work/?id=${id}`, config)
+        .then((response) => setOngoingJobs(response.data))
+        .catch((error) => console.log(error.response))
+    }
+  }
 
-	useEffect(() => {
-		if (id !== null) {
-			fetchOngoingJobs()
-		}
-	}, [dispatch, id])
+  useEffect(() => {
+    if (id !== null) {
+      fetchOngoingJobs()
+    }
+  }, [dispatch, id])
 
-	return (
-		<div className='dashboard-main'>
-			<h3 className='dashboard-title'>Ongoing Jobs</h3>
-			<div className='dashboard-content'>
-				<div className='ongoing-job-area'>
-					{ongoingJobs === null ? (
-						<Spinner />
-					) : ongoingJobs.length === 0 ? (
-						<p>No Ongoing Jobs</p>
-					) : (
-						ongoingJobs.map(job => <RecruiterOngoingJobBlock work={job} fetch={fetchOngoingJobs} />)
-					)}
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className='dashboard-main'>
+      <h3 className='dashboard-title'>Ongoing Jobs</h3>
+      <div className='dashboard-content'>
+        <div className='ongoing-job-area'>
+          {ongoingJobs === null ? (
+            <Spinner />
+          ) : ongoingJobs.length === 0 ? (
+            <p>No Ongoing Jobs</p>
+          ) : (
+            ongoingJobs.map((job) => <RecruiterOngoingJobBlock work={job} fetch={fetchOngoingJobs} />)
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default RecruiterOngoingJob
--- src/components/dashboard/recruiter/ongoing/recruiter/RecruiterOngoingJobBlock.js
+++ src/components/dashboard/recruiter/ongoing/recruiter/RecruiterOngoingJobBlock.js
@@ -1,99 +1,138 @@
-import { Link } from 'react-router-dom'
-import { DateTime } from 'luxon'
-import { FaBookmark, FaMapMarkerAlt } from 'react-icons/fa'
-import { useJobType, useTimeSince } from 'hooks/useJobHook'
-import { useState } from 'react'
-import axios from 'axios'
+import axios from "axios"
+import { useJobType, useTimeSince } from "hooks/useJobHook"
+import { DateTime } from "luxon"
+import { useState } from "react"
+import { FaBookmark, FaMapMarkerAlt } from "react-icons/fa"
+import { Link } from "react-router-dom"
 const url = process.env.REACT_APP_BACKEND_SERVER
 
 const RecruiterOngoingJobBlock = ({ work, fetch }) => {
-	const [curProgress, setCurProgress] = useState('')
-	const { id, created, status, job, seeker } = work
-	const { title, location, type } = job
-	const { picture, expertise, user } = seeker
-	const { username, first_name, last_name } = user
-	const jobType = useJobType(type)
-	const timeSince = useTimeSince(DateTime.fromISO(created))
+  const [curProgress, setCurProgress] = useState("")
+  const { id, created, status, job, seeker } = work
+  const { title, location, type } = job
+  const { picture, expertise, user } = seeker
+  const { username, first_name, last_name } = user
+  const jobType = useJobType(type)
+  const timeSince = useTimeSince(DateTime.fromISO(created))
 
-	const progressReport = async () => {
-		if (curProgress !== '') {
-			const workData = {
-				status: curProgress,
-			}
-			const config = {
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			}
-			await axios
-				.patch(`${url}/jobs/work/${id}/`, workData, config)
-				.then(response => {
-					console.log(response.data)
-					fetch()
-				})
-				.catch(error => console.log(error.data))
-		}
-	}
+  const progressReport = async () => {
+    if (curProgress !== "") {
+      const workData = {
+        status: curProgress,
+      }
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+      await axios
+        .patch(`${url}/jobs/work/${id}/`, workData, config)
+        .then((response) => {
+          console.log(response.data)
+          fetch()
+        })
+        .catch((error) => console.log(error.data))
+    }
+  }
 
-	const progressChange = e => {
-		setCurProgress(e.target.value)
-	}
+  const progressChange = (e) => {
+    setCurProgress(e.target.value)
+  }
 
-	return (
-		<div className='ongoing-job-item'>
-			<div className='ongoing-job-header'>
-				<h5>Title: {title}</h5>
-				<p>
-					<FaMapMarkerAlt /> {location} <FaBookmark /> {jobType}
-				</p>
-			</div>
-			<div className='ongoing-job-content'>
-				<div className='ongoing-job-cards'>
-					<div className='ongoing-card active'>
-						<p>Applied</p>
-					</div>
-					<div className='ongoing-card active'>
-						<p>Accept</p>
-					</div>
-					<div className={status > 1 ? 'ongoing-card active' : 'ongoing-card'}>
-						<p>Prepare</p>
-					</div>
-					<div className={status > 2 ? 'ongoing-card active' : 'ongoing-card'}>
-						<p>Complete</p>
-					</div>
-					<div className={status > 3 ? 'ongoing-card active' : 'ongoing-card'}>
-						<p>Review</p>
-					</div>
-					<div className={status > 4 ? 'ongoing-card active' : 'ongoing-card'}>
-						<p>Done</p>
-					</div>
-				</div>
-				<div className='ongoing-job-progress'>
-					<div className='bar' style={{ width: 20 * status + '%' }}></div>
-				</div>
-			</div>
-			<div className='ongoing-job-footer'>
-				<img src={picture} alt='' width='40px' />
-				<p>
-					Seeker:
-					<Link to={`/user/${username}`}> {first_name + ' ' + last_name}</Link>({expertise})
-				</p>
-				<p>Started: {timeSince}</p>
-			</div>
-			<div className='ongoing-job-footer'>
-				<label for='cars'>Update Job Progress:</label>
-				<select value={curProgress} onChange={progressChange}>
-					<option value=''>-Select-</option>
-					<option value='1'>Accept</option>
-					<option value='2'>Prepare</option>
-					<option value='3'>Complete</option>
-					<option value='4'>Review</option>
-					<option value='5'>Done</option>
-				</select>
-				<button onClick={progressReport}>Update</button>
-			</div>
-		</div>
-	)
+  return (
+    <div className='ongoing-job-item'>
+      <div className='ongoing-job-header'>
+        <h5> {title}</h5>
+        <p>
+          <span>
+            <FaMapMarkerAlt /> {location}
+          </span>{" "}
+          <span>
+            <FaBookmark /> {jobType}
+          </span>
+          <span>$ 300 tk/hour</span>
+        </p>
+      </div>
+      <div className='ongoing-job-content'>
+        <div className='ongoing-job-details-information'>
+          <div className='information-cell'>
+            <p className='title'>Vaccant No</p>
+            <h5 className='title-data'>01</h5>
+          </div>
+          <div className='information-cell'>
+            <div className='seeker-profile'>
+              <div className='seeker-profile-bg'>
+                <img src={picture} alt='' width='40px' />
+                <div>
+                  <h5 className='title-data'>
+                    <Link to={`/user/${username}`}> {first_name + " " + last_name}</Link>
+                  </h5>
+                  <p className='title'>Web Developer</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='information-cell'>
+            <p className='title'>Progress</p>
+            <h5 className='title-data'>Started</h5>
+          </div>
+          <div className='information-cell'>
+            <p className='title'>Work Started</p>
+            <h5 className='title-data'>{timeSince}</h5>
+          </div>
+        </div>
+        <div className='job-tracker-progress-wrapper'>
+          <div className='ongoing-job-cards'>
+            <div className='ongoing-card active'>
+              <p>Applied</p>
+            </div>
+            <div className='ongoing-card active'>
+              <p>Accept</p>
+            </div>
+            <div className={status > 1 ? "ongoing-card active" : "ongoing-card"}>
+              <p>Prepare</p>
+            </div>
+            <div className={status > 2 ? "ongoing-card active" : "ongoing-card"}>
+              <p>Complete</p>
+            </div>
+            <div className={status > 3 ? "ongoing-card active" : "ongoing-card"}>
+              <p>Review</p>
+            </div>
+            <div className={status > 4 ? "ongoing-card active" : "ongoing-card"}>
+              <p>Done</p>
+            </div>
+          </div>
+          <div className='ongoing-job-progress'>
+            <div className='bar' style={{ width: 20 * status + "%" }}></div>
+            <div className='bar mobile-view' style={{ height: 20 * status + "%" }}></div>
+          </div>
+        </div>
+      </div>
+
+      <div className='ongoing-job-footer'>
+        <div className='review-remove-link'>
+          <div className='btn-group'>
+            <span className='btn review'>Review Seeker</span>
+            <span className='btn remove'>Remove Seeker</span>
+          </div>
+        </div>
+        <div className='ongoing-job-update-tracker'>
+          <label for='cars'>Update Job Progress</label>
+          <select value={curProgress} onChange={progressChange}>
+            <option value=''>-Select-</option>
+            <option value='1'>Accept</option>
+            <option value='2'>Prepare</option>
+            <option value='3'>Complete</option>
+            <option value='4'>Review</option>
+            <option value='5'>Done</option>
+          </select>
+          <button className='btn btn-main' onClick={progressReport}>
+            Update
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default RecruiterOngoingJobBlock
