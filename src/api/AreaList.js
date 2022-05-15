0b8c8fc05fe2b757b2a1359cced8ba18dff4c630
Rashed Ahmed <rashed.ahmed@email.com>
2022-05-15 14:32:40 +0600

    make area-list local-api

---
+++ src/api/AreaList.js
@@ -0,0 +1,31 @@
+const AREA_LIST = [
+	{
+		id: 1,
+		name: 'Mohammadpur',
+		bn_name: 'মোহাম্মদপুর',
+		lat: 23.7673346,
+		lng: 90.3571841,
+	},
+	{
+		id: 2,
+		name: 'Dhanmondi',
+		bn_name: 'ধানমন্ডি',
+		lat: 23.746466,
+		lng: 90.376015,
+	},
+	{
+		id: 3,
+		name: 'Mirpur',
+		bn_name: 'মিরপুর',
+		lat: 23.82235,
+		lng: 90.365417,
+	},
+	{
+		id: 4,
+		name: 'Ramna',
+		bn_name: 'রমনা',
+		lat: 23.733023,
+		lng: 90.398384,
+	},
+]
+export default AREA_LIST
--- src/components/home/heroSection/searchBar/SearchBar.js
+++ src/components/home/heroSection/searchBar/SearchBar.js
@@ -1,89 +1,87 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import LocationIcon from "./LocationIcon"
-import SearchBarTag from "./SearchBarTag"
+import AreaList from 'api/AreaList'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import LocationIcon from './LocationIcon'
+import SearchBarTag from './SearchBarTag'
 
 const SearchBar = () => {
-  const navigate = useNavigate()
-  const [districts, setDistricts] = useState([])
-  const [suggestions, setSuggestions] = useState([])
-  const [showSuggestion, setShowSuggestion] = useState(false)
-  const [text, setText] = useState("")
+	const navigate = useNavigate()
+	const [districts, setDistricts] = useState([])
+	const [suggestions, setSuggestions] = useState([])
+	const [showSuggestion, setShowSuggestion] = useState(false)
+	const [text, setText] = useState('')
 
-  useEffect(() => {
-    const loadDistricts = async () => {
-      const response = await axios.get("http://sajeeb45.pythonanywhere.com//api/area/list")
-      setDistricts(response.data)
-      console.log(response.data)
-    }
-    loadDistricts()
-  }, [])
+	useEffect(() => {
+		const loadDistricts = () => {
+			setDistricts(AreaList)
+		}
+		loadDistricts()
+	}, [])
 
-  const onChangeHandler = (text) => {
-    let matches = []
-    if (text.length > 0) {
-      matches = districts.filter((dst) => {
-        const regex = new RegExp(`${text}`, "gi")
-        return dst.name.match(regex)
-      })
-    }
-    if (matches.length > 0) {
-      setSuggestions(matches)
-      setText(text)
-      setShowSuggestion(true)
-    } else {
-      setSuggestions([])
-      setText(text)
-      setShowSuggestion(false)
-    }
-  }
+	const onChangeHandler = text => {
+		let matches = []
+		if (text.length > 0) {
+			matches = districts.filter(dst => {
+				const regex = new RegExp(`${text}`, 'gi')
+				return dst.name.match(regex)
+			})
+		}
+		if (matches.length > 0) {
+			setSuggestions(matches)
+			setText(text)
+			setShowSuggestion(true)
+		} else {
+			setSuggestions([])
+			setText(text)
+			setShowSuggestion(false)
+		}
+	}
 
-  const onSuggestHandler = (text) => {
-    setText(text)
-    setSuggestions([])
-  }
+	const onSuggestHandler = text => {
+		setText(text)
+		setSuggestions([])
+	}
 
-  const getStartedHandler = () => {
-    navigate(`/location/${text}`)
-  }
+	const getStartedHandler = () => {
+		navigate(`/location/${text}`)
+	}
 
-  const setLocation = (data) => {
-    if (data) setText(data)
-  }
+	const setLocation = data => {
+		if (data) setText(data)
+	}
 
-  return (
-    <div className={showSuggestion ? "hero-search-bar active" : "hero-search-bar"}>
-      <div className='search-bar'>
-        <LocationIcon setLocation={(data) => setLocation(data)} />
-        <input
-          type='text'
-          onChange={(e) => onChangeHandler(e.target.value)}
-          onBlur={() => {
-            setTimeout(() => {
-              setShowSuggestion(false)
-            }, 100)
-          }}
-          value={text}
-          placeholder='Where do you live ?'
-        />
-        <button className='btn btn-main' onClick={getStartedHandler}>
-          Get Started
-        </button>
-      </div>
+	return (
+		<div className={showSuggestion ? 'hero-search-bar active' : 'hero-search-bar'}>
+			<div className='search-bar'>
+				<LocationIcon setLocation={data => setLocation(data)} />
+				<input
+					type='text'
+					onChange={e => onChangeHandler(e.target.value)}
+					onBlur={() => {
+						setTimeout(() => {
+							setShowSuggestion(false)
+						}, 100)
+					}}
+					value={text}
+					placeholder='Where do you live ?'
+				/>
+				<button className='btn btn-main' onClick={getStartedHandler}>
+					Get Started
+				</button>
+			</div>
 
-      <div className='auto-complete'>
-        {suggestions &&
-          suggestions.map((sug, i) => (
-            <li key={i} onClick={() => onSuggestHandler(sug.name)}>
-              {sug.name}
-            </li>
-          ))}
-      </div>
+			<div className='auto-complete'>
+				{suggestions &&
+					suggestions.map((sug, i) => (
+						<li key={i} onClick={() => onSuggestHandler(sug.name)}>
+							{sug.name}
+						</li>
+					))}
+			</div>
 
-      <SearchBarTag />
-    </div>
-  )
+			<SearchBarTag />
+		</div>
+	)
 }
 
 export default SearchBar
